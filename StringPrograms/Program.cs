4254ce38683be1b371a12a5603ad2ac73c485eb3
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-03 09:54:46 +0530

    Add project files.

---
+++ StringPrograms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and make use of all string functions in it
+            string myName = "Akshata Sabne";
+            Console.WriteLine(myName);
+            Console.WriteLine(myName[0]);
+            Console.WriteLine(myName.Length);
+            Console.WriteLine(myName.IndexOf('A'));
+            Console.WriteLine(myName.LastIndexOf('a'));
+            Console.WriteLine(myName.ToLower());
+            Console.WriteLine(myName.ToUpper());
+            Console.WriteLine(myName.Contains("Akshata"));
+            Console.WriteLine(myName.Equals("Akshata Sabne"));
+            Console.WriteLine(myName.GetHashCode());
+            Console.WriteLine(myName.Count());
+            string myName1 = "Akshay";
+            string s = string.Concat(myName, myName1);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ StringPrograms/String2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPrograms
+{
+    internal class String2
+    {
+        static void Main(string[] args)
+        {
+            //Write a program to count the number of words in a string
+
+            string line = "I am very Greateful";
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+         //   Console.WriteLine(
+
+            Console.WriteLine(word);
+        }
+    }
+}
